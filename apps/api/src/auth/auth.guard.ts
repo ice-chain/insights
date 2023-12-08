e66a2c181e42b8479f03e4af2ae08f21a4e6d61b
Aisel <aisel.goo@gmail.com>
2023-12-08 17:27:46 +0300

    feat(api): add auth guard

---
+++ apps/api/src/auth/auth.guard.ts
@@ -0,0 +1,54 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Clerk } from '@clerk/clerk-sdk-node';
+import { Request } from 'express';
+
+// TODO: create module https://github.com/devagrawal09/clerk-nest-example/blob/main/api/src/clerk.module.ts
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private clerkService: ReturnType<typeof Clerk>;
+
+  constructor() {
+    this.clerkService = Clerk({
+      secretKey: process.env.CLERK_SECRET_KEY,
+      publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    });
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const authState = await this.clerkService.authenticateRequest({ headerToken: token });
+
+      if (!authState.isSignedIn) return false;
+
+      const auth = authState.toAuth();
+
+      request['auth'] = auth;
+
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    // Note: Clerk stores the clientToken in a cookie
+    // named "__session" for Firebase compatibility
+    const clientToken = request.cookies['__session'];
+
+    return clientToken;
+  }
+}
