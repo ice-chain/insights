f2e872d869b6aa755d756e11a47e81039ec41869
Aisel <aisel.goo@gmail.com>
2024-12-22 18:46:54 +0300

    dev ssl

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,16 +1,18 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
+    https: {},
     proxy: {
       '/api': {
         target: 'http://localhost:3000/',
